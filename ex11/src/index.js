089d1b1abd7f789c558c320608edc569df1880e2
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 20:53:08 +0200

    nth character

---
+++ ex11/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    var lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2];
+    
+    //Only change code below this line
+    
+    
+    
+    //Only change code above this line
+
+    return thirdLetterOfLastName;
+}
+
+console.log(main());
+
+module.exports = main;
