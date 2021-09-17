26268f0be2308fa6532b068683d8032144c072fa
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 11:52:38 +0200

    appending variables to strings

---
+++ ex07/src/index.js
@@ -0,0 +1,18 @@
+function main () {
+    var someAdjective = "the best!";
+    var myString ="Mistral is ";
+    //Only change the code below this line
+
+    myString += someAdjective;
+
+    //Only change the code below this line
+
+
+    return {
+        myString,
+    };
+}
+
+console.log(main());
+
+module.exports = main;
