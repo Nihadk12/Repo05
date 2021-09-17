379107c712555808eac6bee276b6c1335e31cc40
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 12:01:05 +0200

    length

---
+++ ex08/src/index.js
@@ -0,0 +1,18 @@
+function main () {
+    var lastName = "Doe";
+    var lastNameLength;
+    //Only change the code below this line
+
+    lastNameLength = lastName.length;
+
+    //Only change the code below this line
+
+
+    return {
+        lastNameLength,
+    };
+}
+
+console.log(main());
+
+module.exports = main;
