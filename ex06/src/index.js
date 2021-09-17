ced7266e205e501a7de099d47d544e7fe2a4bd4c
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 11:38:31 +0200

    .

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+function main () {
+    var myName = "Nihad";
+    var myString = "Hello, my name is "+ myName +" and I am well!";
+    //Only change the code below this line
+
+
+    //Only change the code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
