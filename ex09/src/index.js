440006c2193a87eb8fe5809ccd782006887d39a2
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 20:16:42 +0200

    firstLetter

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+function main () {
+    var lastName = "Doe";
+    var firstLetterOfLastName = " ";
+    //Only change the code below this line
+
+    firstLetterOfLastName = lastName[0];
+
+    //Only change the code below this line
+
+
+    return {
+        firstLetterOfLastName,
+    };
+}
+
+console.log(main());
+
+module.exports = main;
