b6a3a5d084b683fe43e5c7bf4a18b161f9fe865a
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 11:27:37 +0200

    concatenation

---
+++ ex05/src/index.js
@@ -0,0 +1,15 @@
+function main () {
+    var myString = "Concatenation is the process of appending ";
+    //Only change the code below this line
+
+    myString += "one string to the end of another string.";
+
+    //Only change the code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
