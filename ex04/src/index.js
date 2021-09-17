ea843d147ef49ab2da407128353eac58e3b2fbfb
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 11:22:15 +0200

    commited

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+    var myString = "This is the first string. " + "This is the second string.";
+    //Only change the code below this line
+
+
+    //Only change the code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
