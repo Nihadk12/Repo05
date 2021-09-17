37a34aa6e48ca5e4ea2a6767954e946b930083b4
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 21:00:42 +0200

    ..

---
+++ ex13/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var lastName = "Bezos";
+    
+    //Only change code below this line
+    
+    thirdToLastLetterOfLastName = lastName[lastName.length - 3];
+    
+    //Only change code above this line
+
+    return thirdToLastLetterOfLastName;
+}
+
+console.log(main());
+
+module.exports = main;
