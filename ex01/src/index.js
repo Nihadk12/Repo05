b0fc852c02b908ce732bd72426f7399d22b9d15f
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 10:46:58 +0200

    double quotes

---
+++ ex01/src/index.js
@@ -0,0 +1,18 @@
+function main () {
+    var myFirstString = "I think therefore I \"am\" -- sentient.";
+    var mySecondString = "No, you only \"think\", so you aren't.";
+    //Only change the code below this line
+
+
+    //Only change the code below this line
+
+
+    return{
+        myFirstString,
+        mySecondString
+    };
+}
+
+console.log(main());
+
+module.exports = main;
