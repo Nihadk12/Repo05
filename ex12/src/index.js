38347e1500530c182eea747b08bb5efb9ce09c70
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 20:57:00 +0200

    twelve

---
+++ ex12/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var lastName = "Tesla";
+    
+    //Only change code below this line
+    
+    lastLetterOfLastName = lastName[lastName.length - 1];
+    
+    //Only change code above this line
+
+    return lastLetterOfLastName;
+}
+
+console.log(main());
+
+module.exports = main;
