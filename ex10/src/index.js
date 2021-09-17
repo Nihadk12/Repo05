5efac71ce938989643976441e671db452f6ad6b3
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 20:41:44 +0200

    j u h

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    var myString = "Jello World";
+    
+    myString = "Hello World";
+    //Only change code below this line
+    
+    
+    
+    //Only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
