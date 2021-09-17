27d759b5a17175cfbd9d5119056997836be27052
Nihadk12 <kadic_niho@hotmail.com>
2021-09-17 11:17:47 +0200

    commited

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+    var myString = "FirstLine\n\tSecondLine\nThirdLine";
+    //Only change the code below this line
+
+
+    //Only change the code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
